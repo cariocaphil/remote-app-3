163111f555bb713c48a6bc78122e9b19e28f8dac
cariocaphil <cariocaphil@gmail.com>
2025-02-08 14:12:34 +0100

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack").container.ModuleFederationPlugin;
+
+module.exports = {
+  mode: "production", // Change to "development" when running locally
+  entry: "./src/index.js",
+  devServer: {
+    port: 3002, // Unique port for this remote app
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "remoteApp3", // Unique name for this remote app
+      filename: "remoteEntry.js",
+      exposes: {
+        "./InterestCalculator": "./src/components/InterestCalculator",
+      },
+      shared: { 
+        react: { singleton: true, requiredVersion: "^18.0.0" },
+        "react-dom": { singleton: true, requiredVersion: "^18.0.0" },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
